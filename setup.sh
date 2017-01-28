ad233a528800257401a95d11c3180860ea2987fa
Fernando Flórez <fernando@funciton.com>
2017-01-27 19:20:48 -0500

    Added basic setup script

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+function die() {
+    echo "${@}"
+    exit 1
+}
+
+function clone_repo() {
+    git clone --recursive https://github.com/fernandoflorez/v.git \
+        "${HOME}/.v" || die "Could not clone repo"
+}
+
+function link_files() {
+    ln -s "${HOME}/.v/v/vimrc" "${HOME}/.vimrc" || die "Could not create links"
+}
+
+function install() {
+    if [ ! -e "${HOME}/.v" ]; then
+        clone_repo
+    fi
+    link_files
+}
+
+install
